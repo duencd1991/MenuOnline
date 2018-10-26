94782d5626b44b24087f5779b51241e1bbb3e7d1
duencd <duencd@asiopay.com>
2018-10-26 11:55:17 +0700

    Fix add redux-extension

--- src/helper/store.js
+++ src/helper/store.js
@@ -1,17 +1,23 @@
-import { createStore, applyMiddleware,compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../redux/reducers';
 
 const loggerMiddleware = createLogger();
 
+let storeEnhancers = [];
+if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+  storeEnhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  checkBrowserSupportReduxDevtools = true;
+}
+
 export const store = createStore(
-    rootReducer,
-    compose(applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-    
+  rootReducer,
+  compose(applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  ),
+    ...storeEnhancers
+  )
+
 );
